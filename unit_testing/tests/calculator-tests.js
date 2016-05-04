460aa9c6e0016bccb4b5293169332f05f20b45da
antonkostiuchkov <antonkostiuchkov@gmail.com>
2016-05-04 20:43:01 +0200

    unit_testing

--- unit_testing/tests/calculator-tests.js
+++ unit_testing/tests/calculator-tests.js
@@ -1,18 +1,33 @@
 describe('Calculator', function () {
     var calculator;
 
+    before(function () {
+        console.log("before");
+        calculator = new Calculator;
+    });
+
     beforeEach(function () {
-        calculator = new Calculator();
+        console.log("before each");
     });
 
     describe('Add', function () {
         it("should return the sum of two operands", function () {
             expect(calculator.add(1, 1)).to.be(2);
         });
+
+        it("should return NaN if passed 0 arguments");
     });
     describe('Subtract', function () {
         it("should return the difference of two operands", function () {
             expect(calculator.subtract(4, 2)).to.be(2);
         });
+    });
+
+    afterEach(function () {
+        console.log("after each")
+    });
+
+    after(function () {
+        console.log("after");
     });
 });
