62a429194f43e4c80e676e641f740b49cf3ced76
antonkostiuchkov <antonkostiuchkov@gmail.com>
2016-05-07 10:58:50 +0200

    ang1.5

--- Angular_1.5/movies/module.js
+++ Angular_1.5/movies/module.js
@@ -1,5 +1,17 @@
 (function () {
     "use strict";
 
-    angular.module("movies", []);
+    var module = angular.module("movies", ["ngRoute"]);
+
+    module.config(function ($routeProvider) {
+        $routeProvider
+            .when("/list", {template: "<movie-list></movie-list>"})
+            .when("/about", {template: "<app-about></app-about>"})
+            .otherwise({redirectTo: "/list"});
+    });
+
+    module.component("appAbout", {
+        template: "This is the about page"
+    });
+
 }());
