a6203db9a9bcf16acc0bbb8d0c19f63bab402edf
madkraft <antonkostiuchkov@gmail.com>
2016-05-17 19:13:32 +0200

    falcor

---
+++ unit_testing/tests/twitter-tests.js
@@ -0,0 +1,27 @@
+(function () {
+    "use strict";
+
+    var URL_TEMPLATE = '...';
+
+    twitter.init = function (selector, template) {
+        this.$selection = $(selector);
+        this.template =
+    };
+
+    twitter.getTweets = function (userName, callback) {
+
+    };
+
+    twitter.buildTweets = function (tweets) {
+
+    };
+
+    twitter.displayTweets = function (tweets) {
+
+    };
+
+    twitter.transformDate = function (tweets) {
+
+    };
+
+}(window.twitter = window.twitter || {}, jQuery));
