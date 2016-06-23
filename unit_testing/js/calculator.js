74fe3ddd8c00fe84b19e004d904b8a7f59c98b01
antonkostiuchkov <antonkostiuchkov@gmail.com>
2016-06-23 08:32:31 +0200

    oop-es6

--- unit_testing/js/calculator.js
+++ unit_testing/js/calculator.js
@@ -1,13 +1,43 @@
-var Calculator = (function () {
-    function Calculator() {};
+// var Calculator = (function () {
+//     function Calculator() {};
+//
+//     Calculator.prototype.add = function (operand1, operand2) {
+//         return operand1 + operand2;
+//     };
+//
+//     Calculator.prototype.subtract = function (operand1, operand2) {
+//         return operand1 - operand2;
+//     };
+//
+//     return Calculator;
+// })();
 
-    Calculator.prototype.add = function (operand1, operand2) {
-        return operand1 + operand2;
-    };
 
-    Calculator.prototype.subtract = function (operand1, operand2) {
-        return operand1 - operand2;
-    };
+//////////////////////////////
 
-    return Calculator;
-})();
+var poll = {
+    ping: function (ping) {
+        setTimeout(function () {
+            ping("Pong!");
+        }, 500);
+    }
+};
+
+var view = {
+    $el: $('#main'),
+    show: function () {
+        return this.$el.fadeIn(250).promise();
+    }
+};
+
+var getTemplate = function (path, callback) {
+    return $.ajax({url: path});
+};
+
+function deferredRefresh() {
+    return $.Deferred(function (dfd) {
+        setTimeout(function () {
+            dfd.resolve("Ping!")
+        }, 500);
+    }).promise();
+}
--- unit_testing/tests/calculator-tests.js
+++ unit_testing/tests/calculator-tests.js
@@ -1,33 +1,43 @@
-describe('Calculator', function () {
-    var calculator;
+// describe('Calculator', function () {
+//     var calculator;
+//
+//     before(function () {
+//         console.log("before");
+//         calculator = new Calculator;
+//     });
+//
+//     beforeEach(function () {
+//         console.log("before each");
+//     });
+//
+//     describe('Add', function () {
+//         it("should return the sum of two operands", function () {
+//             expect(calculator.add(1, 1)).to.be(2);
+//         });
+//
+//         it("should return NaN if passed 0 arguments");
+//     });
+//     describe('Subtract', function () {
+//         it("should return the difference of two operands", function () {
+//             expect(calculator.subtract(4, 2)).to.be(2);
+//         });
+//     });
+//
+//     afterEach(function () {
+//         console.log("after each")
+//     });
+//
+//     after(function () {
+//         console.log("after");
+//     });
+// });
 
-    before(function () {
-        console.log("before");
-        calculator = new Calculator;
-    });
+///////////////////////////////////////////////
 
-    beforeEach(function () {
-        console.log("before each");
-    });
-
-    describe('Add', function () {
-        it("should return the sum of two operands", function () {
-            expect(calculator.add(1, 1)).to.be(2);
+describe("Ping", function () {
+    it("should update in 500ms", function () {
+        poll.ping(function (message) {
+            expect(message).to.be("Pong!");
         });
-
-        it("should return NaN if passed 0 arguments");
-    });
-    describe('Subtract', function () {
-        it("should return the difference of two operands", function () {
-            expect(calculator.subtract(4, 2)).to.be(2);
-        });
-    });
-
-    afterEach(function () {
-        console.log("after each")
-    });
-
-    after(function () {
-        console.log("after");
     });
 });
