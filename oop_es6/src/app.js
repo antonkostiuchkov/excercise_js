99f42b6f0cb33e80178377daffed23c51b4915bb
antonkostiuchkov <antonkostiuchkov@gmail.com>
2016-06-24 00:18:49 +0200

    oopes6 get set

--- oop_es6/src/app.js
+++ oop_es6/src/app.js
@@ -1,1 +1,30 @@
-console.log("message");
+class Drone {
+    constructor(id, name) {
+        this._id = id;
+        this.name = name;
+    }
+
+    fly() {
+        console.log("Drone " + this.id + " is flying");
+
+    }
+
+    static getCompany() {
+        console.log("in getCompany");
+
+    }
+
+    get id() {
+        return this._id
+    }
+
+    set id(value) {
+        this._id = value 
+    }
+}
+
+let drone = new Drone(123, 'Flyer');
+let drone2 = new Drone(321, 'Twirl');
+
+drone.fly();
+console.log(drone.id + ': ' + drone.name);
