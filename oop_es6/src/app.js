6982175d4a109b50b07a13f10db7d89c832f4598
antonkostiuchkov <antonkostiuchkov@gmail.com>
2016-06-27 23:29:18 +0200

    finish oopes6

--- oop_es6/src/app.js
+++ oop_es6/src/app.js
@@ -3,6 +3,8 @@ import {fleet} from './fleet-data.js';
 import {FleetDataService} from  './services/fleet-data-service.js';
 import {ApplicationBase} from  './framework/application-base.js';
 import {HomePage} from  './pages/home-page.js';
+import {CarsPage} from  './pages/cars-page.js';
+import {MapPage} from  './pages/map-page.js';
 
 export class App extends ApplicationBase {
     constructor() {
@@ -11,9 +13,9 @@ export class App extends ApplicationBase {
         this.dataService.loadData(fleet);
 
         this.addRoute('Home', new HomePage(), true);
-        this.addRoute('Cars', null);
+        this.addRoute('Cars', new CarsPage());
         this.addRoute('Drones', null);
-        this.addRoute('Map', null);
+        this.addRoute('Map', new MapPage());
     }
 }
 
